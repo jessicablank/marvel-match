2e73e05657ec0e846d71f8c517ba3abf369b9131
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-06-25 16:04:44 -0700

    redirecting to results page thru button on quiz page

--- quiz.js
+++ quiz.js
@@ -1,10 +1,11 @@
 //Quiz!
 //When the User Selects an answer, a function assigns a point value to each answer
 //The points are associated with a character and results are displayed
-
-
+$(function(){
+localStorage.setItem("marvel-match-name", "Thor");
 
 startQuiz();
+endQuiz();
 
 var question1Value = 1;
 
@@ -16,7 +17,9 @@ $("#a2").on("click", function() {
 })
 $("#a3").on("click", function() {
   question1Value = 3;
-})
+});
+
+
 
 //When the User Presses Play, a Function displays the questions
 function startQuiz() {
@@ -28,8 +31,12 @@ function startQuiz() {
   )
 }
 
-//When the start screen is hidden, a Function displays the first question
-function getQuestion() {
-
+//When the user clicks on the results button, they are redirected to the results html page
+function endQuiz() {
+  $(".show-results").on("click", function () {
+    window.location.replace("results.html");
+  }
+  )
 }
 
+})
