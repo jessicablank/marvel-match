afd1923d5aeab16fecd7827b81ce8d3d23d767f9
tober65 <timothy.ober@gmail.com>
2020-06-25 11:01:31 -0700

    Show name and bio on results.html

--- script.js
+++ script.js
@@ -13,5 +13,9 @@ $.ajax({
       "https://gateway.marvel.com/v1/public/characters?name="+ winnerName + "&limit=1&apikey=7d5f2e35ee9376fda0cff9e664d68a3a",
     method: "GET",
   }).then(function (response) {
-    console.log(response);
+    var name = response.data.results[0].name;
+    var bio = response.data.results[0].description;
+
+    $("#name").text(name);
+    $("#bio").text(bio);
   });
